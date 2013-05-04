b2f392d9bbc80224dd8a22f99d47feffd2799448
Alan Hancock <alahanco@iupui.edu>
2013-05-04 15:17:30 -0400

    Added clock css and function, view project
    I have added some design to the clock on the view project page. Also,
    the view project page now updates with information, including resuming
    at the previous start time.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
 ;(function($, window) {
 	
 	var library = new localStorageDB("projectsLibrary");
-	var currentProject = {
-		word: "hello",
-		goodbye: "no"
-	};
+	var currentProject;
+	var currentLastStart;
 	
-	if (library.isNew()) {
-		library.createTable("projects",["name","rate","description"]);
-	}
+//	if (library.isNew()) {
+		library.dropTable("projects");
+		library.createTable("projects",["name","rate","description","lastStart"]);
+//	}
 	
 	library.deleteRows("projects");
+	
+	library.insert("projects",{name: "John's Website", rate: 45, description: "Need it done by Thursday at noon.", lastStart: $.now()});
+	library.commit()
+	currentProject = "John's Website";
+	
 	
 	// instantiate jQTouch	
 	var jQT;
@@ -25,19 +29,20 @@
 		
 		else {
 			library.query("projects", function(p) {
-				$(".projects-list").append('<a id="click-project" href="#" ><li><span>' + p.name + '</span><i class="icon-chevron-right"></i></li></a>');
+				$(".projects-list").append('<a class="click-project" id="' + p.ID + '" href="#view-project" ><li><span>' + p.name + '</span><i class="icon-chevron-right"></i></li></a>');
 			})
 		}
 	};
 	
-	$('#home').bind('pageAnimationEnd', function(event,info) {
+	$('#home').bind('pageAnimationStart', function(event,info) {
 		if (info.direction == "in") {
 			update_projects();
 		}
 	});
 	
-	$('#click-project').bind("clickEvent", function() {
-		console.log("something");
+	$('.click-project').live("click", function(event) {
+//		console.log(event);
+//		console.log(event.currentTarget.attributes);
 	});
 	
 	$('#new-project').bind('pageAnimationEnd', function(event,info) {
@@ -48,10 +53,40 @@
 	
 	$('#edit-project').bind('pageAnimationEnd', function(event,info) {
 		if (info.direction == "in") {
-			alert("edit project in");
+			var $t = $(this);
+			
+			library.query("projects", function(p) {
+				if (p.name == currentProject) {
+					currRate = p.rate;
+					currDesc = p.description;
+				}
+			})
+			
+			$t.find('#project-name').val(currentProject);
+			$t.find('#project-rate').val(currRate);
+			$t.find('#project-description').val(currDesc);
 		}
 	});
 
+	$('#view-project').bind('pageAnimationStart', function(event,info) {
+		if (info.direction == "in") {
+			$("#view-event h1").text(currentProject);
+			
+			library.query("projects", function(p) {
+				if (p.name == currentProject) currentLastStart = p.lastStart;
+			})
+			
+			if (currentLastStart) {
+				clock.setTime( ($.now() - currentLastStart)/1000 );
+				clock.start();
+			}
+		}
+	});
+	
+	$('#view-project .startstop').live('click', function() {
+		
+	})
+	
 	$('#add-project').submit(function(e) {
 		
 		var $t = $(this);
@@ -60,7 +95,7 @@
 		var description = $t.find('#project-description').val();
 		
 		
-		library.insert("projects", {name: name, rate: rate, description: description});
+		library.insert("projects", {name: name, rate: rate, description: description, lastStart: 0});
 		library.commit()
 		
 		
@@ -75,11 +110,33 @@
 		
 	});
 	
-	var clock = $('.clock').FlipClock({
-	
+	$('#edit-project').submit(function(e) {
+		
+		var $t = $(this);
+		var name = $t.find('#project-name').val();
+		var rate = $t.find('#project-rate').val();
+		var description = $t.find('#project-description').val();
+		
+		
+		library.insert("projects", currentProject, {name: name, rate: rate, description: description, lastStart: currentLastStart});
+		library.commit()
+		
+		currentProject = name;
+		
+		
+		console.log( library.query("projects") );
+		console.log( "number of projects: " + library.rowCount("projects") );
+		
+		jQT.goTo('#view-project','slidedown');
+		
+		e.preventDefault();
+		
+		return false;
+		
 	});
 	
+	var clock = $('.clock').FlipClock({ autoStart: false });
+	
 	update_projects();
-	console.log(currentProject);
 	
 }(jQuery, this));
