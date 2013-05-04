d0b575450a2f7e4002f1c0547a48bcdd094a3f41
Alan Hancock <alahanco@iupui.edu>
2013-05-03 20:26:22 -0400

    Forms, functionality for add project
    I added content to the pages of the forms, and the add project form has
    been given some functionality. In addition, the database with
    localStorageDB has been setup for projects.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,44 @@
 ;(function($, window) {
-
+	
+	var library = new localStorageDB("projectsLibrary");
+	
+	if (library.isNew()) {
+		library.createTable("projects",["name","rate","description"]);
+	}
+	
 	// instantiate jQTouch	
 	var jQT;
 	$(function(){ jQT = new $.jQTouch({}); });
+	
+	$('#home').bind('pageAnimationEnd', function(event,info) {
+		if (info.direction == "in") {
+
+		}
+	});
+	
+	$('#add-project').submit(function(e) {
+		
+		var $t = $(this);
+		var name = $t.find('#project-name').val();
+		var rate = $t.find('#project-rate').val();
+		var description = $t.find('#project-description').val();
+		
+		var newProject = new Project( name, rate, description );
+		
+		
+		library.insert("projects", {name: name, rate: rate, description: description});
+		library.commit()
+		
+		
+		console.log( library.query("projects") );
+		console.log( "number of projects: " + library.rowCount("projects") );
+		
+		jQT.goTo('#home','slidedown');
+		
+		e.preventDefault();
+		
+		return false;
+		
+	});
 	
 }(jQuery, this));
