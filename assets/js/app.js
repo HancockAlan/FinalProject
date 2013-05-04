b6902205c8fa09010b3761f2243666c16e42c416
Alan Hancock <alahanco@iupui.edu>
2013-05-03 23:23:23 -0400

    Rough functionality to listing projects
    The functionality of adding project information to the #home screen in
    the form of bullets, later to be buttons, has been added.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,41 @@
 		library.createTable("projects",["name","rate","description"]);
 	}
 	
+	library.deleteRows("projects");
+	
 	// instantiate jQTouch	
 	var jQT;
 	$(function(){ jQT = new $.jQTouch({}); });
 	
+	var update_projects = function() {
+		$(".projects-list").empty();
+		
+		if (library.rowCount("projects") == 0) {
+			$(".projects-list").append('<li><span>Currently no projects</span></li>');
+		}
+		
+		else {
+			library.query("projects", function(p) {
+				$(".projects-list").append('<li><span>' + p.name + ' ' + p.rate + ' ' + p.description + '</span></li>');
+			})
+		}
+	};
+	
 	$('#home').bind('pageAnimationEnd', function(event,info) {
 		if (info.direction == "in") {
+			update_projects();
+		}
+	});
+	
+	$('#new-project').bind('pageAnimationEnd', function(event,info) {
+		if (info.direction == "in") {
 
+		}
+	});
+	
+	$('#edit-project').bind('pageAnimationEnd', function(event,info) {
+		if (info.direction == "in") {
+			alert("edit project in");
 		}
 	});
 	
@@ -22,8 +50,6 @@
 		var name = $t.find('#project-name').val();
 		var rate = $t.find('#project-rate').val();
 		var description = $t.find('#project-description').val();
-		
-		var newProject = new Project( name, rate, description );
 		
 		
 		library.insert("projects", {name: name, rate: rate, description: description});
@@ -40,5 +66,7 @@
 		return false;
 		
 	});
+	
+	update_projects();
 	
 }(jQuery, this));
